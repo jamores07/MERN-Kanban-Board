dcf0b3e91b058fc11047872c268d7121ef6bd312
John A <kayak.stated_0b@icloud.com>
2024-03-14 22:25:39 +0000

    Initiated Login-Register Pages

--- client/src/api/StatApi.js
+++
@@ -1,7 +0,0 @@
-import axiosClient from "./axiosClient";
-
-const statApi = {
-    stat: axiosClient.get('/stat')
-}
-
-export default statApi
--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { useState, useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import authUtils from '../../utils/authUtils'
+import Loading from '../common/Loading'
+import SideBar from '../common/SideBar'
+import { setUser } from '../../redux/features/userSlice'
 
 const AppLayout = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const user = await authUtils.isAuthenticated()
+      if (!user) {
+        navigate('/login')
+      } else {
+        // save user
+        dispatch(setUser(user))
+        setLoading(false)
+      }
+    }
+    checkAuth()
+  }, [navigate])
+
   return (
-    <div>
-        <Outlet />
-    </div>
+    loading ? (
+      <Loading fullHeight />
+    ) : (
+      <Box sx={{
+        display: 'flex'
+      }}>
+        <SideBar />
+        <Box sx={{
+          flexGrow: 1,
+          p: 1,
+          width: 'max-content'
+        }}>
+          <Outlet />
+        </Box>
+      </Box>
+    )
   )
 }
 
