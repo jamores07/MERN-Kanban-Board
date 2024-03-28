3dae827a2ec7f04863efd3d60e85858e0493b799
John A <kayak.stated_0b@icloud.com>
2024-03-28 00:36:06 +0000

    Added MirageJS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { makeServer } from './server/server'
+
+/* MirageJS */
+if (process.env.NODE_ENV === 'development') {
+   makeServer({ environment: 'development' });
+  }
+
+
 
 function App() {
-  
 
   return (
     <>
+    <div>
+
+    </div>
     </>
   )
 }
